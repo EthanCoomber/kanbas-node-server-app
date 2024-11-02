55a38bc38c46d0896934eb0f680750e5f537c830
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-11-01 20:16:51 -0400

    3.6.1

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -5,16 +5,12 @@ let todos = [
   { id: 4, title: "Task 4", completed: true },
 ];
 export default function WorkingWithArrays(app) {
-  app.get("/lab5/todos", (req, res) => {
-    const { completed } = req.query;
-    if (completed !== undefined) {
-      const completedBool = completed === "true";
-      const completedTodos = todos.filter((t) => t.completed === completedBool);
-      res.json(completedTodos);
-      return;
-    }
-    res.json(todos);
+  app.post("/lab5/todos", (req, res) => {
+    const newTodo = { ...req.body, id: new Date().getTime() };
+    todos.push(newTodo);
+    res.json(newTodo);
   });
+
   app.get("/lab5/todos/:id", (req, res) => {
     const { id } = req.params;
     const todo = todos.find((t) => t.id === parseInt(id));
@@ -29,6 +25,7 @@ export default function WorkingWithArrays(app) {
     todos.push(newTodo);
     res.json(todos);
   });
+
   app.get("/lab5/todos/:id/delete", (req, res) => {
     const { id } = req.params;
     const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
