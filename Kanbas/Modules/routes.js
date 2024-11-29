1697bcd2c9ca4d104a975779e74c0366c35e4b31
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-11-29 16:34:36 -0500

    5.2.5

--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -10,15 +10,19 @@ export default function ModuleRoutes(app) {
     res.send(newModule);
   });
 
-  app.delete("/api/modules/:moduleId", (req, res) => {
+  app.delete("/api/modules/:moduleId", async (req, res) => {
     const { moduleId } = req.params;
-    modulesDao.deleteModule(moduleId);
-    res.sendStatus(204);
+    const status = await modulesDao.deleteModule(moduleId);
+    res.send(status);
+ 
   });
-  app.put("/api/modules/:moduleId", (req, res) => {
+  app.put("/api/modules/:moduleId", async (req, res) => {
+    console.log("TESTI");
     const { moduleId } = req.params;
     const moduleUpdates = req.body;
-    modulesDao.updateModule(moduleId, moduleUpdates);
-    res.sendStatus(204);
+    console.log('in update', moduleId, moduleUpdates);
+    const status = await modulesDao.updateModule(moduleId, moduleUpdates);
+    console.log('status', status);
+    res.send(status);
   });
 }
